e7036def57d8873f19d71fa2ef58fa2fc31e56ee
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-03-07 18:14:45 +0000

    Add project files.

---
+++ Week6 Lab/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week6_Lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //referencing database
+       
+        Model1Container db = new Model1Container(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+
+        //runs on window load for startup ations
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var result = from a in db.Bands
+                         select a;
+
+            lbxBands.ItemsSource = result.ToList();
+          
+
+
+        }
+
+
+        //reacts to selecting an album from the bands listbox
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //determine whatss is selected
+            Band selectedBand = lbxBands.SelectedItem as Band;
+           // int id = (int)lbxBands.SelectedValue;
+
+            //check it's not null
+            if (selectedBand != null)
+            {
+                
+                var result = from a in db.Albums
+                             where a.Band.Id == selectedBand.Id
+                             select a.Name;
+
+                //update second listbox
+                lbxAlbums.ItemsSource = result.ToList();
+
+            }
+
+
+        }
+    }
+
+
+   
+
+
+}
---
+++ Week6 Lab/PartialClasses/Band.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_Lab
+{
+   public partial class Band
+    {
+
+
+        public override string ToString()
+        {
+            return $"Name of Band/Artist:  {Name}  Year Formed:  {YearFormed}";
+        }
+
+
+    }
+}
